9ffc6c55eb47619585879abe2e2e5ea027fdf947
ianmc94 <156915380+ianmc94@users.noreply.github.com>
2025-10-09 16:43:34 +0100

    Week 4

--- Week2/JS/main.js
+++
@@ -1,4 +0,0 @@
-function myFunction(){
-    console.log("arrived at my function")
-    window.location = "https://www.bbc.co.uk/news/articles/cly72j21wp5o"
-}
---
+++ public/JS/fundamentals.js
@@ -0,0 +1,34 @@
+/*
+Fundamentals Code
+*/
+
+// Window Alert 
+function initialized()
+{
+window.alert("hello World");
+document.getElementById("printhere").innerHTML = "hello World";
+
+let x = "20"  + "25"
+Document.getElementById("datatype").innerHTML = x;
+
+// boolean and conditionals 
+x =5;
+y =9;
+
+
+if ( x !=y ){document.getElementById("booleans").innerHTML = "it's true" ,x, "!= ", y }
+
+if (xx ==22) 
+{
+ document.getElementById("boolean1").innerHTML = "Its true" + x +
+}
+
+
+}
+
+
+// Function
+function dateTime()
+{
+    document.getElementById("datetime").innerHTML = Date();
+}
